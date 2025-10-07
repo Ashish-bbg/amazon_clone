fb93011641916735cd2f152eac9306b5e94aeaac
Ashish-bbg <ashishbbgsingh@gmail.com>
2025-10-07 15:42:12 +0530

    Initial backend setup

---
+++ db.js
@@ -0,0 +1,25 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+});
+
+// Testing connection once at startup
+
+async function testConn() {
+  try {
+    console.log("Establishing MySQL connection...");
+    const conn = await pool.getConnection();
+    console.log("MySQL connected successfully");
+    conn.release();
+  } catch (error) {
+    console.log("MySQL connection failed", error.message);
+    process.exit(1);
+  }
+}
+
+await testConn();
+
+export default pool;
---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import pool from "./db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+//  test route
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
