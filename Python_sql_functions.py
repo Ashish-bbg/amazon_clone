7da7e413ec9fe9039683cfd17a431344d7dad469
Sharma <sumit.bz.sharma@accenture.com>
2025-10-13 16:25:40 +0530

    product routes created.

--- Python_sql_functions.py
+++ Python_sql_functions.py
@@ -2,7 +2,8 @@ import mysql.connector as sql
 import os
 from dotenv import load_dotenv
 from urllib.parse import urlparse
-import mysql.connector as sql
+from flask import Flask, jsonify, request
+
 
 load_dotenv()
 db_url = os.getenv("MYSQL_URL")
@@ -12,20 +13,42 @@ db_config = {
     "user": parsed.username,
     "password": parsed.password,
     "database": parsed.path.lstrip("/"),
-    "port": parsed.port or 3306
+    "port": parsed.port or 3306,
 }
 
-class SQL_Connection():
-    
+
+class SQL_Connection:
+
     con = sql.connect(**db_config)
     cursor = con.cursor()
     print("Connected to Railway MySQL!")
 
-    def create_table(self,table_name):
-        #table_name=self.table_name
-        SQL_Connection.cursor.execute(f""" CREATE TABLE IF NOT EXISTS {table_name} (prodid VARCHAR(10) NOT NULL PRIMARY KEY,title VARCHAR(600),imgurl VARCHAR(500),producturl VARCHAR(500),reviews INT,price FLOAT,isbestseller TINYINT(1),boughtlastmonth  INT,categoryname VARCHAR(100),stars FLOAT);""")
+    def create_table_products(self, table_name):
+        # table_name=self.table_name
+        SQL_Connection.cursor.execute(
+            f" CREATE TABLE IF NOT EXISTS {table_name} (prodid VARCHAR(10) NOT NULL PRIMARY KEY,title VARCHAR(600),imgurl VARCHAR(500),producturl VARCHAR(500),reviews INT,price FLOAT,isbestseller TINYINT(1),boughtlastmonth  INT,categoryname VARCHAR(100),stars FLOAT);"
+        )
         SQL_Connection.cursor.execute(f"""desc {table_name};""")
-        for i in SQL_Connection.cursor:
-            print(i)
-    
-    
+        return jsonify(SQL_Connection.cursor.execute(f"""desc {table_name};"""))
+
+    def home():
+        return jsonify({"message": "Welcome to AMAZON"})
+
+    def get_products(self, table_name):
+        query = f"SELECT * FROM {table_name};"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        columns = [desc[0] for desc in self.cursor.description]
+        data = [dict(zip(columns, row)) for row in rows]
+        return data
+
+    def get_product_by_id(self, prod_id):
+        try:
+            query = f"""Select * from products where prodid="{prod_id}";"""
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            columns = [desc[0] for desc in self.cursor.description]
+            data = [dict(zip(columns, row)) for row in rows]
+            return data
+        except Exception as e:
+            return {"error": str(e)}
--- products.py
+++ products.py
@@ -1,8 +1,38 @@
 import Python_sql_functions as py
+from flask import Flask,jsonify,request
+
+
+
+app=Flask(__name__)
 
 conn=py.SQL_Connection()
 
-conn.create_table("demo1")
+@app.route('/')
+def home():
+    return py.SQL_Connection.home()
+
+@app.route("/api/table_description")
+def table_description():
+     return conn.create_table_products("demo1")
+
+@app.route('/api/products')
+def get_products():
+    return jsonify(conn.get_products('products'))
+
+@app.route('/api/id=<prod_id>', methods=['GET'])
+def get_product_by_id(prod_id):
+    data=conn.get_product_by_id(prod_id)
+    if "error" in data:
+        return jsonify(data), 500
+    elif data:
+        return jsonify(data)
+    else:
+        return jsonify({"Error":"Product ID not found!!"}), 404
 
 
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
 
